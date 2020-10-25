0f528c39c880779ba9c9e01dc13e1bf63055e6b1
Liudmila Nizkova <liudmila_nizkova@epam.com>
2020-10-25 23:02:36 +0300

    add changes

--- selenium/src/main/java/com/epam/googlecalculator/driver/DriverSingleton.java
+++ selenium/src/main/java/com/epam/googlecalculator/driver/DriverSingleton.java
@@ -11,24 +11,19 @@ public class DriverSingleton {
     private DriverSingleton(){}
 
     public static WebDriver getDriver(){
-        if (null == driver) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+        if (null == driver){
+            switch (System.getProperty("browser")){
+                case "firefox": {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                default: {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+            }
+            driver.manage().window().maximize();
         }
-//        if (null == driver){
-//            switch (System.getProperty("browser")){
-//                case "firefox": {
-//                    WebDriverManager.firefoxdriver().setup();
-//                    driver = new FirefoxDriver();
-//                }
-//                default: {
-//                    WebDriverManager.chromedriver().setup();
-//                    driver = new ChromeDriver();
-//                }
-//            }
-//            driver.manage().window().maximize();
-//        }
-        driver.manage().window().maximize();
         return driver;
     }
 
